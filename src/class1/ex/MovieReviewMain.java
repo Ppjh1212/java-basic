9d0e95d120ed292a42d92d9c9b412114959cd331
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-04-03 20:57:48 +0900

    1

---
+++ src/class1/ex/MovieReviewMain.java
@@ -0,0 +1,12 @@
+package class1.ex;
+
+public class MovieReviewMain {
+    public static void main(String[] args) {
+        MovieReview parkpark = new MovieReview();
+
+        parkpark.title = "파크파크";
+        parkpark.review = "정말 재밌었어요.";
+
+        System.out.println("영화 제목 : " + parkpark.title + ", 리뷰 : " + parkpark.review);
+    }
+}
