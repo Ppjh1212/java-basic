dad7296dd30432af23a87857101ef3fd2ce50bea
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-04-14 14:02:17 +0900

    7

--- src/com/helloshop/order/Order.java
+++ src/com/helloshop/order/Order.java
@@ -4,14 +4,12 @@ import com.helloshop.product.Product;
 import com.helloshop.user.User;
 
 public class Order {
-    User user;
-    Product product;
+    User user; //어떤 사용자가 주문했는지 유제에 대한 정보
+    Product product; //사용자가 어떤 제품을 주문했는지 제품에 대한 정보
 
 
-    public Order(User user, Product product){
+    public Order(User user, Product product){ //어떤 사용자가 어떤 제품을 주문했는지
         this.user = user;
         this.product = product;
     }
-
-
 }
--- src/com/helloshop/order/OrderService.java
+++ src/com/helloshop/order/OrderService.java
@@ -1,13 +1,10 @@
 package com.helloshop.order;
-
 import com.helloshop.product.Product;
 import com.helloshop.user.User;
-
 public class OrderService {
-
     public void order() {
-        User user = new User();
-        Product product = new Product();
+        User user = new User(); //유저Id, 이름
+        Product product = new Product(); //제품Id, 가격
         Order order = new Order(user, product);
     }
 }
